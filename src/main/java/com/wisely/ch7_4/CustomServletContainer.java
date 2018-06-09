f8d00f3bd505412e429273356a750bff3896dbe3
zhixiaotianzhuo <zhixiaotianzhuo@163.com>
2018-06-09 15:09:34 +0800

    重定向成功，但页面显示失败

--- src/main/java/com/wisely/ch7_4/CustomServletContainer.java
+++ src/main/java/com/wisely/ch7_4/CustomServletContainer.java
@@ -1,7 +1,6 @@
 package com.wisely.ch7_4;
 
 import java.util.concurrent.TimeUnit;
-
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.embedded.ErrorPage;
